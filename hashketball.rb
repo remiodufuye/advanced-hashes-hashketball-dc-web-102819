ef8870bb4edba48d2476253a3fe2d15c561fa25f
remiodufuye <remi.odufuye@gmail.com>
2019-10-26 09:28:44 -0400

    mid review

--- hashketball.rb
+++ hashketball.rb
@@ -1,131 +1,133 @@
 
 # Write your code here!
+# on your laptop run gem install PRY 
 
 require 'pry' 
 
             def game_hash
                 {
-                :away => {:team_name =>  "Charlotte Hornets" ,
-                        :colors => ["Turquoise" , "Purple"] , 
-                        :players => [
-                            { :player_name => "Jeff Adrien" ,
-                                :number => 4 ,
-                                :shoe => 18 , 
-                                :points => 10 , 
-                                :rebounds => 1 , 
-                                :assists => 1  , 
-                                :steals => 2 , 
-                                :blocks => 7 ,
-                                :slam_dunks => 2 
-                            } , 
-                            {
-                                :player_name => "Bismack Biyombo" ,
-                                :number => 0 ,
-                                :shoe => 16 , 
-                                :points => 12 , 
-                                :rebounds => 4 , 
-                                :assists => 7  , 
-                                :steals => 22 , 
-                                :blocks => 15 ,
-                                :slam_dunks => 10 
-                            } ,
-                            {
-                                :player_name => "DeSagna Diop" ,
-                                :number => 2 ,
-                                :shoe => 14 , 
-                                :points => 24 , 
-                                :rebounds => 12 , 
-                                :assists => 12  , 
-                                :steals => 4 , 
-                                :blocks => 5 ,
-                                :slam_dunks => 5 
-                            },
-                            {
-                                :player_name => "Ben Gordon" ,
-                                :number => 0 ,
-                                :shoe => 16 , 
-                                :points => 12 , 
-                                :rebounds => 4 , 
-                                :assists => 7  , 
-                                :steals => 22 , 
-                                :blocks => 15 ,
-                                :slam_dunks => 10 
-                            },
-                            {
-                                :player_name => "Kemba Walker" ,
-                                :number => 33 ,
-                                :shoe => 15 , 
-                                :points => 6 , 
-                                :rebounds => 12 , 
-                                :assists => 12  , 
-                                :steals => 7 , 
-                                :blocks => 5 ,
-                                :slam_dunks => 12 
-                            }
-                        ] 
-                    
+                    # Home hash is below 
+                                :home => {:team_name => "Brooklyn Nets" ,
+                                        :colors => ["Black" , "White"] , 
+                                        :players => [
+                                            { :player_name => "Alen Anderson" ,
+                                                :number => 0 ,
+                                                :shoe => 16 , 
+                                                :points => 22 , 
+                                                :rebounds => 12 , 
+                                                :assists => 12  , 
+                                                :steals => 3 , 
+                                                :blocks => 1 ,
+                                                :slam_dunks => 1 
+                                            } , 
+                                            {
+                                                :player_name => "Reggie Evans" ,
+                                                :number => 30 ,
+                                                :shoe => 14 , 
+                                                :points => 12 , 
+                                                :rebounds => 12 , 
+                                                :assists => 12  , 
+                                                :steals => 12 , 
+                                                :blocks => 12 ,
+                                                :slam_dunks => 7 
+                                            } ,
+                                            {
+                                                :player_name => "Brook Lopez" ,
+                                                :number => 11 ,
+                                                :shoe => 17 , 
+                                                :points => 17 , 
+                                                :rebounds => 19 , 
+                                                :assists => 10  , 
+                                                :steals => 3 , 
+                                                :blocks => 1 ,
+                                                :slam_dunks => 15 
+                                            },
+                                            {
+                                                :player_name => "Mason Plumlee" ,
+                                                :number => 1 ,
+                                                :shoe => 19 , 
+                                                :points => 26 , 
+                                                :rebounds => 11 , 
+                                                :assists => 6  , 
+                                                :steals => 3 , 
+                                                :blocks => 8 ,
+                                                :slam_dunks => 5 
+                                            },
+                                            {
+                                                :player_name => "Jason Terry" ,
+                                                :number => 31 ,
+                                                :shoe => 15 , 
+                                                :points => 19, 
+                                                :rebounds => 2 , 
+                                                :assists => 2  , 
+                                                :steals => 4 , 
+                                                :blocks => 11 ,
+                                                :slam_dunks => 1
+                                            }
+                                        ]
+                                    
+                                },
+                                :away => {:team_name =>  "Charlotte Hornets" ,
+                                :colors => ["Turquoise" , "Purple"] , 
+                                :players => [
+                                    { :player_name => "Jeff Adrien" ,
+                                        :number => 4 ,
+                                        :shoe => 18 , 
+                                        :points => 10 , 
+                                        :rebounds => 1 , 
+                                        :assists => 1  , 
+                                        :steals => 2 , 
+                                        :blocks => 7 ,
+                                        :slam_dunks => 2 
+                                    } , 
+                                    {
+                                        :player_name => "Bismack Biyombo" ,
+                                        :number => 0 ,
+                                        :shoe => 16 , 
+                                        :points => 12 , 
+                                        :rebounds => 4 , 
+                                        :assists => 7  , 
+                                        :steals => 22 , 
+                                        :blocks => 15 ,
+                                        :slam_dunks => 10 
+                                    } ,
+                                    {
+                                        :player_name => "DeSagna Diop" ,
+                                        :number => 2 ,
+                                        :shoe => 14 , 
+                                        :points => 24 , 
+                                        :rebounds => 12 , 
+                                        :assists => 12  , 
+                                        :steals => 4 , 
+                                        :blocks => 5 ,
+                                        :slam_dunks => 5 
+                                    },
+                                    {
+                                        :player_name => "Ben Gordon" ,
+                                        :number => 0 ,
+                                        :shoe => 16 , 
+                                        :points => 12 , 
+                                        :rebounds => 4 , 
+                                        :assists => 7  , 
+                                        :steals => 22 , 
+                                        :blocks => 15 ,
+                                        :slam_dunks => 10 
+                                    },
+                                    {
+                                        :player_name => "Kemba Walker" ,
+                                        :number => 33 ,
+                                        :shoe => 15 , 
+                                        :points => 6 , 
+                                        :rebounds => 12 , 
+                                        :assists => 12  , 
+                                        :steals => 7 , 
+                                        :blocks => 5 ,
+                                        :slam_dunks => 12 
+                                    }
+                                ] 
+                            
 
-                } ,  # Home hash is below 
-                :home => {:team_name => "Brooklyn Nets" ,
-                        :colors => ["Black" , "White"] , 
-                        :players => [
-                            { :player_name => "Alen Anderson" ,
-                                :number => 0 ,
-                                :shoe => 16 , 
-                                :points => 22 , 
-                                :rebounds => 12 , 
-                                :assists => 12  , 
-                                :steals => 3 , 
-                                :blocks => 1 ,
-                                :slam_dunks => 1 
-                            } , 
-                            {
-                                :player_name => "Reggie Evans" ,
-                                :number => 30 ,
-                                :shoe => 14 , 
-                                :points => 12 , 
-                                :rebounds => 12 , 
-                                :assists => 12  , 
-                                :steals => 12 , 
-                                :blocks => 12 ,
-                                :slam_dunks => 7 
-                            } ,
-                            {
-                                :player_name => "Brook Lopez" ,
-                                :number => 11 ,
-                                :shoe => 17 , 
-                                :points => 17 , 
-                                :rebounds => 19 , 
-                                :assists => 10  , 
-                                :steals => 3 , 
-                                :blocks => 1 ,
-                                :slam_dunks => 15 
-                            },
-                            {
-                                :player_name => "Mason Plumlee" ,
-                                :number => 1 ,
-                                :shoe => 19 , 
-                                :points => 26 , 
-                                :rebounds => 11 , 
-                                :assists => 6  , 
-                                :steals => 3 , 
-                                :blocks => 8 ,
-                                :slam_dunks => 5 
-                            },
-                            {
-                                :player_name => "Jason Terry" ,
-                                :number => 31 ,
-                                :shoe => 15 , 
-                                :points => 19, 
-                                :rebounds => 2 , 
-                                :assists => 2  , 
-                                :steals => 4 , 
-                                :blocks => 11 ,
-                                :slam_dunks => 1
-                            }
-                        ]
-                    
-                  }
+                        }
 
                 }
 
@@ -134,7 +136,62 @@ require 'pry'
 
 
 
+    def num_points_scored(players_name)
+        
+        # get access to all the players 
+        game_hash.keys.map do |loc|
+        new_array_of_players = []
+        binding.pry 
+        end 
+        # lookup the player 
 
+        # get those stats !
+
+    end 
+
+
+
+    def shoe_size()
+    end 
+
+
+
+    def team_colors()
+    end 
+
+
+
+    def team_names()
+    end 
+
+    def player_numbers() 
+    end 
+
+    def player_stats()
+    end 
+
+    def big_shoe_rebounds() 
+    end 
+
+
+     ############# BONUS #########
+
+     def most_points_scored()
+     end 
+
+
+     def winning_team() 
+     end 
+
+
+     def player_with_longest_name() 
+     end 
+
+
+     ########## SUPER BONUS ####
+
+     def long_name_steals_a_ton()
+     end 
 
 
 
